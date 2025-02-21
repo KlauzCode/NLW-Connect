5446d384a19ee874a838ddc6940e7c8b47cb5aab
Flavio Santos <comercialflaviosantos@gmail.com>
2025-02-21 02:17:57 -0300

    Aula 2 | Interface, componentes e formularios

--- DevStage/src/components/icon-button.tsx
+++ DevStage/src/components/icon-button.tsx
@@ -1,17 +1,14 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import { ReactNode } from "react"
+import { type  ComponentProps, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
-interface IconButtonProps {
-    children: ReactNode
-}
+interface IconButtonProps extends ComponentProps<'button'> {}
 
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({className, ...props}: IconButtonProps) {
     return (
-        // biome-ignore lint/a11y/useButtonType: <explanation>
-        <button className="p-1.5 bg-gray-500 text-blue rounded-md  cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
-        >
-            {props.children}
-        </button>
+        <button 
+        className={twMerge("p-1.5 bg-gray-500 text-blue rounded-md  cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900")}
+        {...props}            
+        />
     )
 }
